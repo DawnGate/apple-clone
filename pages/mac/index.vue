0863c86870d5513699f88c94bff6ca31f6a74a70
DawnGate <lam99quyen@gmail.com>
2024-01-29 07:09:54 +0700

    feat: update mac chapternav

--- pages/mac/index.vue
+++ pages/mac/index.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
 
 const chapterNavItems = [
   {
@@ -10,73 +10,87 @@ const chapterNavItems = [
     width: 79,
   },
   {
-    name: 'macbook-air',
-    icon: '/icons/pages/mac/homepage/chapternav/macbookair_light_large.svg',
-    title: 'Macbook Air',
+    name: 'macbook-pro',
+    icon: '/icons/pages/mac/homepage/chapternav/macbook_pro_light_large.svg',
+    title: 'Macbook Pro',
     isNew: true,
-    width: 79,
+    width: 85,
   },
   {
-    name: 'macbook-air',
-    icon: '/icons/pages/mac/homepage/chapternav/macbookair_light_large.svg',
-    title: 'Macbook Air',
+    name: 'imac',
+    icon: '/icons/pages/mac/homepage/chapternav/imac_light_large.svg',
+    title: 'iMac',
     isNew: true,
-    width: 79,
+    width: 43,
   },
   {
-    name: 'macbook-air',
-    icon: '/icons/pages/mac/homepage/chapternav/macbookair_light_large.svg',
-    title: 'Macbook Air',
-    isNew: true,
-    width: 79,
+    name: 'mac-mini',
+    icon: '/icons/pages/mac/homepage/chapternav/mac_mini_light_large.svg',
+    title: 'Mac Mini',
+    width: 28,
   },
   {
-    name: 'macbook-air',
-    icon: '/icons/pages/mac/homepage/chapternav/macbookair_light_large.svg',
-    title: 'Macbook Air',
-    isNew: true,
-    width: 79,
+    name: 'mac-pro',
+    icon: '/icons/pages/mac/homepage/chapternav/mac_pro_light_large.svg',
+    title: 'Mac Pro',
+    width: 35,
   },
   {
-    name: 'macbook-air',
-    icon: '/icons/pages/mac/homepage/chapternav/macbookair_light_large.svg',
-    title: 'Macbook Air',
-    isNew: true,
-    width: 79,
+    name: 'mac-compare',
+    icon: '/icons/pages/mac/homepage/chapternav/mac_compare_light_large.svg',
+    title: 'Compare',
+    width: 45,
   },
   {
-    name: 'macbook-air',
-    icon: '/icons/pages/mac/homepage/chapternav/macbookair_light_large.svg',
-    title: 'Macbook Air',
-    isNew: true,
-    width: 79,
+    name: 'mac-accessories',
+    icon: '/icons/pages/mac/homepage/chapternav/mac_accessories_light_large.svg',
+    title: 'Accessories',
+    width: 34,
+  },
+  {
+    name: 'mac-os',
+    icon: '/icons/pages/mac/homepage/chapternav/mac_os_light_large.svg',
+    title: 'Sonoma',
+    width: 35,
+  },
+  {
+    name: 'shop-mac',
+    icon: '/icons/pages/mac/homepage/chapternav/displays_light_large.svg',
+    title: 'Shop Mac',
+    width: 103,
+  },
+  {
+    name: 'Displays',
+    icon: '/icons/pages/mac/homepage/chapternav/displays_light_large.svg',
+    title: 'Displays',
+    width: 72,
   },
 ]
 
 const scrollItemsRef = ref<HTMLDivElement>()
 
 const shouldShowLeft = ref(false)
-const shouldShowRight = ref(true)
+const shouldShowRight = ref(false)
 
 const timeoutShowPaddles = ref(null)
 
 const handleItemsScroll = (e: UIEvent) => {
   if (!scrollItemsRef.value) return
-  const currentWidth = scrollItemsRef.value.clientWidth - 34 * 2
-  const offsetLeft = scrollItemsRef.value.offsetLeft
+  const scrollWidth = scrollItemsRef.value.scrollWidth
+  const currentWidth = scrollItemsRef.value.clientWidth
+  const scrollLeft = scrollItemsRef.value.scrollLeft
 
   let updateShowPaddles = {
     left: false,
     right: true,
   }
 
-  // TODO: debounce
-  if (offsetLeft === 0) {
+  if (scrollLeft < 0.5) {
     updateShowPaddles = {
       left: false,
       right: true,
     }
-  } else if (currentWidth === offsetLeft) {
+  } else if (currentWidth + scrollLeft > scrollWidth - 0.5) {
     updateShowPaddles = {
       left: true,
       right: false,
@@ -96,12 +110,45 @@ const handleItemsScroll = (e: UIEvent) => {
     timeoutShowPaddles
   )
 }
+
+const handleClickScrollButton = (isRight: boolean) => {
+  if (!scrollItemsRef.value) return
+  scrollItemsRef.value.scrollTo({
+    left:
+      (isRight ? -1 : 1) * scrollItemsRef.value.clientWidth +
+      scrollItemsRef.value.scrollLeft,
+    behavior: 'smooth',
+  })
+}
+
+const updateShowRightPaddle = () => {
+  if (scrollItemsRef.value) {
+    const currentWidth = scrollItemsRef.value.clientWidth
+    const scrollWidth = scrollItemsRef.value.scrollWidth
+    if (currentWidth < scrollWidth) {
+      shouldShowRight.value = true
+    }
+  }
+}
+
+onMounted(() => {
+  updateShowRightPaddle()
+  window.addEventListener('resize', () => {
+    debounce(updateShowRightPaddle, 200, timeoutShowPaddles)
+  })
+})
+
+onUnmounted(() => {
+  if (timeoutShowPaddles.value) {
+    clearTimeout(timeoutShowPaddles.value)
+  }
+})
 </script>
 <template>
   <nav id="chapternav" class="chapternav bg-[#fafafc] py-6">
     <div class="chapternav-wrapper relative">
       <ul
-        class="chapternav-items mx-[34px]"
+        class="chapternav-items mx-[34px] text-center"
         ref="scrollItemsRef"
         @scroll="handleItemsScroll"
       >
@@ -125,7 +172,13 @@ const handleItemsScroll = (e: UIEvent) => {
         </li>
       </ul>
       <div class="chapternav-paddles">
-        <button class="chapternav-paddle chapternav-paddle-left">
+        <button
+          class="chapternav-paddle chapternav-paddle-left"
+          :class="{
+            show: shouldShowLeft,
+          }"
+          @click="handleClickScrollButton(true)"
+        >
           <span class="chapternav-paddle-left-icon">
             <svg
               width="7"
@@ -140,7 +193,13 @@ const handleItemsScroll = (e: UIEvent) => {
             </svg>
           </span>
         </button>
-        <button class="chapternav-paddle chapternav-paddle-right">
+        <button
+          class="chapternav-paddle chapternav-paddle-right"
+          :class="{
+            show: shouldShowRight,
+          }"
+          @click="handleClickScrollButton(false)"
+        >
           <span class="chapternav-paddle-right-icon">
             <svg
               width="7"
@@ -166,6 +225,7 @@ const handleItemsScroll = (e: UIEvent) => {
   --chapternav-link-color-hover: rgb(0, 0, 0);
   --chapter-new-color: rgb(182, 68, 0);
   --scroll-container-border-color: rgb(210, 210, 215);
+  --scroll-container-paddle-fade-duration: 150ms;
 }
 
 .chapternav-wrapper {
@@ -233,6 +293,14 @@ const handleItemsScroll = (e: UIEvent) => {
     display: flex;
     justify-content: center;
     align-items: center;
+
+    // condition
+    opacity: 0;
+    transition: opacity var(--scroll-container-paddle-fade-duration) ease-out;
+  }
+
+  .chapternav-paddle.show {
+    opacity: 1;
   }
 
   .chapternav-paddle.chapternav-paddle-left {
